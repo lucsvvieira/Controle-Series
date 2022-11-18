b0f2db02583d7ece5ef2a8398d445709d0fe5caf
Lucas Vieira <vieira07lucas@gmail.com>
2022-11-18 17:20:47 -0300

    Lidando com Request e Response

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         $series = [
             'The Walking Dead',
             'Breaking Bad',
