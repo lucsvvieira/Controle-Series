56ab0b08db81ca2459470e54a6bf6199e3619cec
Lucas Vieira <vieira07lucas@gmail.com>
2022-11-22 15:46:41 -0300

    Instalando o bootstrap

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }} - Controle de Séries</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
